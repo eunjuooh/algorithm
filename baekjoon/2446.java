bea84965a2bf0514c3739b2c58615ff9b5e7b523
oheunju <loveeeeunju@gmail.com>
2020-09-11 21:25:11 +0900

    백준 2446

---
+++ baekjoon/2446.java
@@ -0,0 +1,45 @@
+﻿import java.util.Scanner;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        
+        int num = sc.nextInt();
+        
+        int maxNum = num * 2 - 1;
+        int star = maxNum;
+        int space = 0;
+        
+        for(int i = 1; i <= maxNum; i++)
+        {
+            if(space > 0)
+            {
+                for(int k = 1; k <= space; k++)
+                {
+                    System.out.print(" ");
+                }
+            }
+            
+            for(int j = 1; j <= star; j++)
+            {
+                System.out.print("*");
+            }
+            
+            System.out.println();
+            
+            if(i < num)
+            {
+                star -= 2;
+                space += 1;
+            }
+            else
+            {
+                star += 2;
+                space -= 1;
+            }
+        }
+    }
+    
+}
